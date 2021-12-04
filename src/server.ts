0acabce3706007543b197b6df8f619f7a9444d17
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-04 16:40:33 -0300

    Add categories route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/categories", categoriesRoutes);
+
 app.get("/", (request, response) => {
   return response.json("Hello World");
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
 });
 
 app.listen(3333, () => console.log("Server is running"));
